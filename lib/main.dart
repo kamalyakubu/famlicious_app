9a2a0210c6e31e10a711c14b45abece34b3c203a
Kamarudeen Yakubu <48481194+kamalyakubu@users.noreply.github.com>
2021-10-18 15:19:26 +0000

    Added create account page

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
+import 'package:famlicious_app/views/auth/create_account_view.dart';
 import 'package:famlicious_app/views/home/home_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
-      // DevicePreview(
-      //   enabled: !kReleaseMode,
-      //   builder: (context) => const MyApp(),
-      // ),
-      const MyApp());
+    // DevicePreview(
+    //   enabled: !kReleaseMode,
+    //   builder: (context) => const MyApp(),
+    // ),
+MyApp()  );
 }
 
 class MyApp extends StatelessWidget {
@@ -24,19 +28,36 @@ class MyApp extends StatelessWidget {
       builder: DevicePreview.appBuilder,
       title: 'Famlicious',
       theme: ThemeData(
-          scaffoldBackgroundColor:const Color.fromRGBO(249, 251, 252, 1),
-          cardColor: Colors.white,
-          appBarTheme: const AppBarTheme(
-              backgroundColor: Colors.white,
-              elevation: 0,
-              actionsIconTheme: IconThemeData(color: Colors.black),
-              titleTextStyle: TextStyle(
-                  color: Colors.black,
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold),),),
+        scaffoldBackgroundColor: const Color.fromRGBO(249, 251, 252, 1),
+        cardColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          actionsIconTheme: IconThemeData(color: Colors.black),
+          titleTextStyle: TextStyle(
+              color: Colors.black, fontSize: 25, fontWeight: FontWeight.bold),
+        ),
+        textTheme: const TextTheme(
+          bodyText1: TextStyle(color: Colors.black),
+          bodyText2: TextStyle(color: Colors.black),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          floatingLabelBehavior: FloatingLabelBehavior.never,
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.black),
+          ),
+          labelStyle: TextStyle(color: Colors.black),
+        ),
+        buttonTheme: const ButtonThemeData(
+          colorScheme: ColorScheme.dark(primary: Colors.white),
+          textTheme: ButtonTextTheme.primary
+        ),
+      ),
       darkTheme: ThemeData(
-        textTheme: TextTheme(bodyText1: TextStyle(color: Colors.white),
-        bodyText2: TextStyle(color: Colors.grey)),
+        textTheme: const TextTheme(
+          bodyText1: TextStyle(color: Colors.white),
+          bodyText2: TextStyle(color: Colors.grey),
+        ),
         scaffoldBackgroundColor: Colors.black,
         cardColor: Colors.grey.shade900,
         appBarTheme: const AppBarTheme(
@@ -50,9 +71,21 @@ class MyApp extends StatelessWidget {
           titleTextStyle: TextStyle(
               color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
         ),
+        inputDecorationTheme: const InputDecorationTheme(
+          floatingLabelBehavior: FloatingLabelBehavior.never,
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.black),
+          ),
+          labelStyle: TextStyle(color: Colors.black),
+        ),
+
+          buttonTheme: const ButtonThemeData(
+          colorScheme: ColorScheme.light(primary: Colors.black),
+          textTheme: ButtonTextTheme.primary
+        ),
       ),
       themeMode: ThemeMode.system,
-      home: const HomeView(),
+      home:  CreateAccountView(),
     );
   }
 }
