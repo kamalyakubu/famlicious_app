7b42ed872b92f56b5f16e62d3753a6133be3474e
Kamarudeen Yakubu <48481194+kamalyakubu@users.noreply.github.com>
2021-10-22 03:26:04 +0000

    Authenticate user with email and password

--- lib/managers/auth_manager.dart
+++ lib/managers/auth_manager.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:famlicious_app/services/file_upload_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class AuthManager with ChangeNotifier {
@@ -10,34 +10,32 @@ class AuthManager with ChangeNotifier {
   static final FirebaseFirestore _firebaseFirestore =
       FirebaseFirestore.instance;
   final FileUploadService _fileUploadService = FileUploadService();
-  String _message = '';
+  String _message = 'hello';
   bool _isLoading = false;
-  String get message => _message;
-  bool get isLoading => _isLoading;
   CollectionReference userCollection = _firebaseFirestore.collection("users");
 
-  
+  String get message => _message; //getter
+  bool get isLoading => _isLoading; //getter
 
   setMessage(String message) {
+    //setter
     _message = message;
-
     notifyListeners();
   }
 
-  setIsLoading(bool isLoading) {
-    _isLoading = isLoading;
-
+  setIsLoading(bool loading) {
+    //setter
+    _isLoading = loading;
     notifyListeners();
   }
 
-  //Authentication with email and password
+  //email, password
   Future<bool> createNewUser(
-      {
-        required String name,
-        required String email,
+      {required String name,
+      required String email,
       required String password,
       required File imageFile}) async {
-    _isLoading = true;
+    setIsLoading(true);
     bool isCreated = false;
     await _firebaseAuth
         .createUserWithEmailAndPassword(email: email, password: password)
@@ -46,29 +44,64 @@ class AuthManager with ChangeNotifier {
           file: imageFile, userUid: userCredential.user!.uid);
 
       if (photoUrl != null) {
-        //Add user info to firestore (name,email,photo,uid,createdAt)
         await userCollection.doc(userCredential.user!.uid).set({
           "name": name,
           "email": email,
           "picture": photoUrl,
           "createdAt": FieldValue.serverTimestamp(),
-          "userId": userCredential.user!.uid
+          "user_id": userCredential.user!.uid
         });
         isCreated = true;
       } else {
-        setMessage('Image upload failed');
+        setMessage('Image upload failed!');
         isCreated = false;
       }
+      setIsLoading(false); //set to false
     }).catchError((onError) {
       setMessage('$onError');
       isCreated = false;
-      setIsLoading(false);
+      setIsLoading(false); //set to false
     }).timeout(const Duration(seconds: 60), onTimeout: () {
-      setMessage('Please check your internet connection');
+      setMessage('Please check your internet connection.');
       isCreated = false;
-      setIsLoading(false);
+      setIsLoading(false); //set to false
     });
-
     return isCreated;
+  }
+
+  Future<bool> loginUser(
+      {required String email, required String password}) async {
+    bool isSuccessful = false;
+    await _firebaseAuth
+        .signInWithEmailAndPassword(email: email, password: password)
+        .then((userCredential) {
+      if (userCredential.user != null) {
+        isSuccessful = true;
+      } else {
+        isSuccessful = false;
+        setMessage('Could not log you in!');
+      }
+    }).catchError((onError) {
+      setMessage('$onError');
+      isSuccessful = false;
+    }).timeout(const Duration(seconds: 60), onTimeout: () {
+      setMessage('Please check your internet connection.');
+      isSuccessful = false;
+    });
+    return isSuccessful;
+  }
+
+  Future<bool> sendResetLink(String email) async {
+    bool isSent = false;
+    await _firebaseAuth.sendPasswordResetEmail(email: email).then((_) {
+      isSent = true;
+    }).catchError((onError) {
+      setMessage('$onError');
+      isSent = false;
+    }).timeout(const Duration(seconds: 60), onTimeout: () {
+      setMessage('Please check your internet connection.');
+      isSent = false;
+    });
+    return isSent;
   }
 }
